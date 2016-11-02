993f262dea041c2071a0c0f7835d6a149042fc7b
Andre Fonseca <afbfonseca@gmail.com>
2016-11-02 12:03:42 +0000

    Add two simulation modes
    - Real world simulation
    - Euler method

--- TPC03/js/skydiver.js
+++ TPC03/js/skydiver.js
@@ -3,7 +3,8 @@ var Skydiver = function(position, velocity, acceleration) {
 	this.position = position
 	this.velocity = velocity
 	this.acceleration = acceleration
-	this.diameter = 10
+	this.diameter = 20
+	this.total_time = 0
 }
 
 Skydiver.prototype.draw = function() {
@@ -11,10 +12,10 @@ Skydiver.prototype.draw = function() {
 	ellipse(this.position.x, this.position.y, this.diameter, this.diameter)
 }
 
-Skydiver.prototype.move = function(delta_time) {
+Skydiver.prototype.move = function(delta) {
 
-	this.velocity.add(p5.Vector.mult(this.acceleration, delta_time / 1000))
-	this.position.add(p5.Vector.mult(this.velocity, delta_time / 1000))
+	this.velocity.add(p5.Vector.mult(this.acceleration, delta / 1000))
+	this.position.add(p5.Vector.mult(this.velocity, delta / 1000))
 	this.acceleration.mult(0)
 }
 
@@ -36,61 +37,78 @@ var frames = {
 var engine = {
 
 	initial_time: 0,
-	total_time: 0,
+	time: 0,
 	pause: false
 }
 
-var skydiver
 var GRAVITY
+var INITIAL_HEIGHT = 1000
 var SIMULATION = 0
 var EULER = 1
-var MODE = SIMULATION
+var MODE = EULER
 var VERBOSE = true
+var skydiver_01
+var skydiver_02
 
 function setup() {
 
 	createCanvas(400, 1000)
-	skydiver = new Skydiver(createVector(width / 2, 0), createVector(0, 0), createVector(0, 0))
-	GRAVITY = createVector(0, 9.81)
+	skydiver_01 = new Skydiver(createVector(width * 1 / 4, 0), createVector(0, 0), createVector(0, 0))
+	skydiver_02 = new Skydiver(createVector(width * 3 / 4, 0), createVector(0, 0), createVector(0, 0))
+	GRAVITY = createVector(0, -9.81)
 }
 
 function draw() {
 
+	if (engine.pause) {
+		return
+	}
+
 	frames.current = millis()
 	frames.delta = frames.current - frames.previous
 	frames.previous = frames.current
-	engine.total_time = frames.current - engine.initial_time
+	engine.time = (frames.current - engine.initial_time) / 1000
 
 	background('#626770')
 
-	if (MODE === EULER) {
-		euler()
-	} else {
-		simulate()
-	}
-
 	// update object
-	skydiver.draw()
+	skydiver_01.draw()
+	skydiver_02.draw()
+
+	// calculate forces
+	skydiver_01 = apply_force(skydiver_01, GRAVITY)
+	skydiver_02 = apply_force(skydiver_02, GRAVITY)
+
+	skydiver_01 = euler(skydiver_01, engine.time)
+	skydiver_02 = simulate(skydiver_02, frames.delta)
+
 
 	// verbose
 	if (!VERBOSE) {
 		return
 	}
 
-	s = skydiver.verbose() + `Total time ${(engine.total_time / 1000).toFixed(2)} s`
 	textSize(10)
-	text(s, 5, 10)
+	s = skydiver_01.verbose() + `Total time ${(engine.time).toFixed(2)} s`
+	text(s, 1, 10)
+	s = skydiver_02.verbose() + `Total time ${(engine.time).toFixed(2)} s`
+	text(s, width / 2, 10)
 }
 
-function euler() {
+function euler(object, time) {
 
+	object.velocity.y = Math.abs(0 + object.acceleration.y * time)
+	x = INITIAL_HEIGHT + 0.5 * object.acceleration.y * Math.pow(time, 2)
+	object.position.y = INITIAL_HEIGHT - x
+	object.acceleration.mult(0)
+	return object
 }
 
-function simulate() {
+function simulate(object, delta) {
 
-	// calculate forces
-	skydiver = apply_force(skydiver, GRAVITY)
-	skydiver.move(frames.delta)
+	object.acceleration.y *= -1
+	object.move(delta)
+	return object
 }
 
 function apply_force(object, force) {
