887e0dadd8f543d59cf622567cee958ada374190
Andre Fonseca <afbfonseca@gmail.com>
2016-11-14 19:19:52 +0000

    standardjs lint

--- TPC03/js/engine.js
+++ TPC03/js/engine.js
@@ -1,63 +1,58 @@
-var Engine = function() {
+var Engine = function () {
+  GRAVITY = createVector(0, -9.81)
+  AIR = {
+    coefficient: 1,
+    density: 1.29
+  }
+  WATER = {
+    coefficient: 1,
+    density: 1000
+  }
 
-	GRAVITY = createVector(0, -9.81);
-	AIR = {
-		coefficient: 1,
-		density: 1.29
-	};
-	WATER = {
-		coefficient: 1,
-		density: 1000
-	};
-
-	this.initial_time = 0;
-	this.time = 0;
-	this.pause = false;
-};
+  this.initial_time = 0
+  this.time = 0
+  this.pause = false
+}
 
 /**
  * Calculate object's motion based on Euler calculation
  * Similar to skydiver's move
  */
-Engine.prototype.euler = function(object, time) {
+Engine.prototype.euler = function (object, time) {
+  object.velocity.y = 0 + object.acceleration.y * time
+  object.position.y = INITIAL_ALTITUDE + 0.5 * object.acceleration.y * Math.pow(time, 2)
 
-	object.velocity.y = 0 + object.acceleration.y * time;
-	object.position.y = INITIAL_ALTITUDE + 0.5 * object.acceleration.y * Math.pow(time, 2);
+  if (object.position.y - object.height * 10 / 2 <= 0) {
+    object.position.y = object.height * 10 / 2
+    object.velocity.y = 0
+  } else {
+    object.flight_time = (millis() - object.flight_time) / 1000
+  }
 
-	if (object.position.y - object.height * 10 / 2 <= 0) {
-		object.position.y = object.height * 10 / 2;
-		object.velocity.y = 0;
-	} else {
-		object.flight_time = (millis() - object.flight_time) / 1000;
-	}
-
-	object.acceleration.mult(0);
-	return object;
-};
+  object.acceleration.mult(0)
+  return object
+}
 
 /**
  * Simulate object's motion in the environment
  */
-Engine.prototype.simulate = function(object, delta) {
-
-	object.move(delta);
-	return object;
-};
+Engine.prototype.simulate = function (object, delta) {
+  object.move(delta)
+  return object
+}
 
 /**
  * Apply an external force to the object
  */
-Engine.prototype.apply_force = function(object, force) {
-
-	var f = p5.Vector.div(force, object.mass);
-	object.acceleration.add(f);
-	return object;
-};
+Engine.prototype.apply_force = function (object, force) {
+  var f = p5.Vector.div(force, object.mass)
+  object.acceleration.add(f)
+  return object
+}
 
 /**
  * Get friction vector
  */
-Engine.prototype.get_friction = function(velocity, coefficient, density, area) {
-
-	return p5.Vector.mult(velocity.copy(), velocity.copy().mag() * coefficient * -0.5 * density * area);
+Engine.prototype.get_friction = function (velocity, coefficient, density, area) {
+  return p5.Vector.mult(velocity.copy(), velocity.copy().mag() * coefficient * -0.5 * density * area)
 }
